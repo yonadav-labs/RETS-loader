0a2035aca285bbdd9bb32da737ee55f39422df81
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-07-21 20:48:36 +0530

    add models

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from general.models import *
+
+admin.site.register(Property)
+admin.site.register(PropertyAttribute)
+admin.site.register(Photo)
+admin.site.register(MlsAgent)
+admin.site.register(MlsOffice)
+admin.site.register(OpenHouse)
+admin.site.register(PropertyRoom)
+admin.site.register(WntDataProvHistory)
