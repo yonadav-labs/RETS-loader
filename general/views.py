3dd9cd8b6ac8a0c5f286ac307128a07fb469ee97
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-07-25 03:46:41 +0530

    add pagination

--- general/views.py
+++ general/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from general.models import *
 from general.serializers import *
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
 class PropertyViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
+    pagination_class = StandardResultsSetPagination
     queryset = Property.objects.all()
 
 
